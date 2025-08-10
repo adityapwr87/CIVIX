05a3373d5c53a7ef6e972ed3263a37ce29b6d41a
adityapwr87 <162857466+adityapwr87@users.noreply.github.com>
2025-08-10 22:26:33 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Set up the base URL for the backend API
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://civix-2-backend.onrender.com/api" });
 
 // Automatically attach token from localStorage
 API.interceptors.request.use(
