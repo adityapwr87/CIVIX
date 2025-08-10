777cb45bcbf9dd647e8b491ea40b31bbdcb45ebb
adityapwr87 <162857466+adityapwr87@users.noreply.github.com>
2025-08-10 22:31:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://civix-1-frontend-2.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   },
